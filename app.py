46ffbd03b68d75caaf1549d05a3bfc2601b3e67c
Failra <88475627+Failra@users.noreply.github.com>
2025-06-03 11:43:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ dogName = st.text_input("Enter your dog's name: ")
 
 dogAge = st.number_input("Enter your dog's age: ")
 
-if st.button("Calculate Dog Year's for", dogName):
+if st.button("Calculate Dog Year's"):
     if dogName and dogAge:
         dogYears = dogAge * 7
         st.write(dogName, "is", dogYears, "dog years old")
